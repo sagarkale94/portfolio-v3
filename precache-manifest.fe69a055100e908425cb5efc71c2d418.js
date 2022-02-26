b779059bcd886f20b1e8e0739bf87396fcac64fb
Sagar Kale <46244365+sagarkale94@users.noreply.github.com>
2022-02-26 17:55:17 +0530

    Add files via upload

---
+++ precache-manifest.fe69a055100e908425cb5efc71c2d418.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16851506c7ec0547de8268a5f592d16a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4bc625dec52aea7e5835",
+    "url": "/portfolio/static/css/2.50a5f423.chunk.css"
+  },
+  {
+    "revision": "7d89420aad18e046c31b",
+    "url": "/portfolio/static/css/main.948206b9.chunk.css"
+  },
+  {
+    "revision": "4bc625dec52aea7e5835",
+    "url": "/portfolio/static/js/2.c26653d0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.c26653d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d89420aad18e046c31b",
+    "url": "/portfolio/static/js/main.5149a88e.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "f0f0447d9c9b237944eca49ba95d74af",
+    "url": "/portfolio/static/media/adm.f0f0447d.png"
+  },
+  {
+    "revision": "5739fd8206ebeab886ad00a8ba519bf0",
+    "url": "/portfolio/static/media/bg.5739fd82.png"
+  },
+  {
+    "revision": "549baffe2026ace963dfd6261b0a8838",
+    "url": "/portfolio/static/media/brigs.549baffe.png"
+  },
+  {
+    "revision": "4b41ec9fd8d98977e2c0efba21fedb3a",
+    "url": "/portfolio/static/media/dms.4b41ec9f.png"
+  },
+  {
+    "revision": "b027bf2640268c5c7dd49414342cbb4c",
+    "url": "/portfolio/static/media/fitke.b027bf26.png"
+  },
+  {
+    "revision": "652db732d908942f87ee207a5e7ddacd",
+    "url": "/portfolio/static/media/ikshu.652db732.png"
+  },
+  {
+    "revision": "32f757275b6102192de0da8ad70267d2",
+    "url": "/portfolio/static/media/lagna.32f75727.png"
+  },
+  {
+    "revision": "74b41650e48bfc0e29fdf2c61c614131",
+    "url": "/portfolio/static/media/ngrx.74b41650.svg"
+  },
+  {
+    "revision": "3d1fe72a5a9717c74b61268a5d8c97c7",
+    "url": "/portfolio/static/media/pic.3d1fe72a.jpg"
+  },
+  {
+    "revision": "5c7392a91f8cd5d997137f6b4a36870b",
+    "url": "/portfolio/static/media/redux.5c7392a9.svg"
+  },
+  {
+    "revision": "ade12638984a3f8638c369f033ed2096",
+    "url": "/portfolio/static/media/styledComp.ade12638.png"
+  },
+  {
+    "revision": "77b8e9b1455ae03acd6a0092d5a547f0",
+    "url": "/portfolio/static/media/sw.77b8e9b1.png"
+  }
+]);
