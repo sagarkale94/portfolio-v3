1cb158313495be70489b7c62628638c61aa43254
Sagar Kale <46244365+sagarkale94@users.noreply.github.com>
2022-02-26 18:16:53 +0530

    Add files via upload

---
+++ precache-manifest.71b2816e1c360735bfecfd556e2bb037.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49d8169f2822a6cd23d00194addbf5b5",
+    "url": "/sagarkale94/portfolio-v3/index.html"
+  },
+  {
+    "revision": "4bc625dec52aea7e5835",
+    "url": "/sagarkale94/portfolio-v3/static/css/2.50a5f423.chunk.css"
+  },
+  {
+    "revision": "87f4e21d0fbffb1c12c0",
+    "url": "/sagarkale94/portfolio-v3/static/css/main.948206b9.chunk.css"
+  },
+  {
+    "revision": "4bc625dec52aea7e5835",
+    "url": "/sagarkale94/portfolio-v3/static/js/2.c26653d0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sagarkale94/portfolio-v3/static/js/2.c26653d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87f4e21d0fbffb1c12c0",
+    "url": "/sagarkale94/portfolio-v3/static/js/main.16d97241.chunk.js"
+  },
+  {
+    "revision": "836929b2dcdf19a7189e",
+    "url": "/sagarkale94/portfolio-v3/static/js/runtime-main.8f764240.js"
+  },
+  {
+    "revision": "f0f0447d9c9b237944eca49ba95d74af",
+    "url": "/sagarkale94/portfolio-v3/static/media/adm.f0f0447d.png"
+  },
+  {
+    "revision": "5739fd8206ebeab886ad00a8ba519bf0",
+    "url": "/sagarkale94/portfolio-v3/static/media/bg.5739fd82.png"
+  },
+  {
+    "revision": "549baffe2026ace963dfd6261b0a8838",
+    "url": "/sagarkale94/portfolio-v3/static/media/brigs.549baffe.png"
+  },
+  {
+    "revision": "4b41ec9fd8d98977e2c0efba21fedb3a",
+    "url": "/sagarkale94/portfolio-v3/static/media/dms.4b41ec9f.png"
+  },
+  {
+    "revision": "b027bf2640268c5c7dd49414342cbb4c",
+    "url": "/sagarkale94/portfolio-v3/static/media/fitke.b027bf26.png"
+  },
+  {
+    "revision": "652db732d908942f87ee207a5e7ddacd",
+    "url": "/sagarkale94/portfolio-v3/static/media/ikshu.652db732.png"
+  },
+  {
+    "revision": "32f757275b6102192de0da8ad70267d2",
+    "url": "/sagarkale94/portfolio-v3/static/media/lagna.32f75727.png"
+  },
+  {
+    "revision": "74b41650e48bfc0e29fdf2c61c614131",
+    "url": "/sagarkale94/portfolio-v3/static/media/ngrx.74b41650.svg"
+  },
+  {
+    "revision": "3d1fe72a5a9717c74b61268a5d8c97c7",
+    "url": "/sagarkale94/portfolio-v3/static/media/pic.3d1fe72a.jpg"
+  },
+  {
+    "revision": "5c7392a91f8cd5d997137f6b4a36870b",
+    "url": "/sagarkale94/portfolio-v3/static/media/redux.5c7392a9.svg"
+  },
+  {
+    "revision": "ade12638984a3f8638c369f033ed2096",
+    "url": "/sagarkale94/portfolio-v3/static/media/styledComp.ade12638.png"
+  },
+  {
+    "revision": "77b8e9b1455ae03acd6a0092d5a547f0",
+    "url": "/sagarkale94/portfolio-v3/static/media/sw.77b8e9b1.png"
+  }
+]);
